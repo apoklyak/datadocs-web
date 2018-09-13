33fa03d02eafc62574f4074b0a8d5af178b431c4
Bhavesh Shah <bhavesh.shah@brevitaz.com>
2018-09-13 16:36:56 +0530

    Fixed issue of 'greyed-out' state at the beginning, insert service file.

--- src/js/modules/main/directives/uploads.js
+++ src/js/modules/main/directives/uploads.js
@@ -263,8 +263,19 @@ define(['./module', 'common', 'notifications-utils', 'lodash'], function (direct
 
                         checkFilesBeforeUpload(items);
 
-                        const {data: filesUploadData} =
-                            await $http.post('/api/files/upload/prepare', {count: _.size(items), fileSize: _.sum(items, function(item) { return item.file.size; })})
+                          await $http.post('/api/files/upload/prepare', {count: _.size(items), fileSize: _.sum(items, function(item) {
+                                                                                                                     item.preUpload=true;
+                                                                                                                     return item.file.size; })})
+                                .then(response => {
+                                    let filesUploadData = response.data;
+                                    items.forEach((item, index) => {
+                                       const fileUpload = filesUploadData[index];
+                                       for (let key in fileUpload) {
+                                           item[key] = fileUpload[key];
+                                       }
+                                       item.preUpload = false;
+                                   });
+                                })
                                 .catch(error => {
                                     if(error.status == 509){
                                         _.each(items, item => {
@@ -279,12 +290,6 @@ define(['./module', 'common', 'notifications-utils', 'lodash'], function (direct
                                         })
                                     }
                                 });
-                        items.forEach((item, index) => {
-                            const fileUpload = filesUploadData[index];
-                            for (let key in fileUpload) {
-                                item[key] = fileUpload[key];
-                            }
-                        });
                     }
                     // For GCS uploader - bulk request for GCS links/filenames before uploading
                     if (UploaderService.isGCSUploader) {
@@ -355,6 +360,8 @@ define(['./module', 'common', 'notifications-utils', 'lodash'], function (direct
                     }
 
                     $scope.getUnderNameStatus = function (item) {
+                        if(item.preUpload)
+                            return `Part 1 of 3 - Uploading (${item.progress}%)...`;
                         let transition = item.isUploaded && !item.ingesting && !item.isCancel && !item.verifying;
 
                         if (!item.isError && item.isUploading || transition) {
